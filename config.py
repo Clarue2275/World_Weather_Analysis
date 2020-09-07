0cd0c9d09ddf98f55faa04246b120d1e2c8812cc
Clarue2275 <68675578+Clarue2275@users.noreply.github.com>
2020-09-07 15:20:14 -0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+# Google API Key
+g_key = "AIzaSyA5qu3R-YQeOmky51lUUNo-O0LGXaQCLDI"
+
+weather_api_key="3aeb10d09861664035f43f525eaf98d4"
+
+# Google API Key
+#g_key = "AIzaSyA36-5GnUzNapmhuyxV_QbSk8DypuQOyUM"
